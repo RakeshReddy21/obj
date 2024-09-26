fc0eef49e096bfd30ae4552c66cfcaf6a3f9f641
RakeshReddy21 <reddykr21@gmail.com>
2024-09-26 10:21:31 +0530

    object numbers

---
+++ obj.js
@@ -0,0 +1,35 @@
+// ------> Objects
+
+const obj1 = { a: 1, b: 2 };
+const obj2 = { c: 3, d: 4 };
+
+const obj3=Object.assign(obj1,obj2)
+console.log(obj3)
+
+
+
+
+
+const objs = { a: 1, b: 2, c: 3, d: 4 };
+let x=Object.values(objs)
+console.log(x)
+let b=x.map((a)=>{return a})
+console.log(b)
+let c=b.reduce((a,b)=>{return a+b})
+console.log("Sum up the values of obj's properties using reduce is " + c)
+
+
+
+
+const obj = { a: 1, b: 2, c: 3, d: 4 }
+newobj={};
+Object.keys(obj).forEach(a=>{
+    if(obj[a]>2){
+        newobj[a]=obj[a]
+    }
+});
+console.log(newobj)
+
+
+
+
